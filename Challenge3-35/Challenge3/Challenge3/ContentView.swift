579868e748dccb3afa6b8c39f7d6f1fbbc206bc8
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-08 13:16:28 -0300

    Fixed with new version

--- Challenge3-35/Challenge3/Challenge3/ContentView.swift
+++ Challenge3-35/Challenge3/Challenge3/ContentView.swift
@@ -14,155 +14,157 @@
  Generate a range of questions based on the user’s settings.
  Show the player how many questions they got correct at the end of the game, then offer to let them play again. */
 
-
 import SwiftUI
-
-
 
 struct ContentView: View {
     
-    struct PerguntaResposta {
-        var fator1: Int
-        var fator2: Int
+    struct QuestionAndAnswer {
+        var operand1: Int
+        var operand2: Int
         
         var answer: Int {
-            fator1 * fator2
+            operand1 * operand2
         }
-        var textoQuestao: String {
-            "\(fator1) * \(fator2)"
+        var questionText: String {
+            "\(operand1) * \(operand2)"
         }
     }
     
-    @State private var chosenTable = 2
-    @State private var numberQuestions = [5, 10, 15, 20]
-    @State private var chosenNumberQuestions = 0
+    @State private var multiplicationTable = 2
+    @State private var numberOfQuestions = [5, 10, 15, 20]
+    @State private var numberOfQuestion = 0
     
-    @State private var questionsBank = [String:Int]()
-    @State private var answersBank = [Int]()
+    @State private var questionArray = [String]()
+    @State private var answerArray = [Int]()
     
-    @State private var userAnswer = 0
-    @State private var correctAnswer = 0
+    @State private var userAnswer: Int = 0
+    @State private var answer = 0
     
     @State private var currentQuestion = 0
-    @State private var displayQuestion = 0
+    @State private var displayQuestion = " "
     
     @State private var score = 0
     
-    @State private var isGameActive = false
-    @State private var isSettingActive = true
+    @State private var playingGame = false
+    @State private var configuringGame = true
     
     @State private var showAlert = false
     @State private var alertMessage = ""
     @State private var gameOver = false
     
-    //    let startGame: (Int) -> Void
-    //
-    //    init(startGame: @escaping (Int) -> Void = { _ in }) {
-    //        self.startGame = startGame
-    //    }
-    
     var body: some View {
         NavigationStack {
             Form {
-                if isGameActive {
-                    GameView(chosenNumberQuestions: chosenNumberQuestions)
-                } else {
-                    GameSettingsView(startGame: startGame)
+                if configuringGame {
+                    Section {
+                        Picker("Selecione tabuada", selection: $multiplicationTable) {
+                            ForEach(2..<13, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }
+                    } header: {
+                        Text("Selecione o valor")
+                    }
+                    Section {
+                        Picker("Quantas perguntas quer responder?", selection: $numberOfQuestion) {
+                            ForEach(numberOfQuestions, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    } header: {
+                        Text("Quantidade questões")
+                    }
+                    Button("Iniciar!") {
+                        configureGame()
+                    }
+                }
+                if playingGame {
+                    Section {
+                        Text(displayQuestion)
+                        Section {
+                            TextField("Resposta usuário", value: $userAnswer, format: .number)
+                            Button("Checar resposta") {
+                                checkAnswer()
+                            }
+                        }
+                        Section {
+                            Text("Pontuação = \(score)")
+                        }
+                    }
+                    .onAppear(perform: setQuestion)
+                    .alert(alertMessage, isPresented: $showAlert) {
+                        Button("Ok") {
+                            if gameOver {
+                                startNewGames()
+                            } else {
+                                
+                            }
+                        }
+                    }
                 }
             }
             .navigationTitle("JogoTabulada")
         }
-        
-        func startGame(with chosenNumberQuestions: Int) {
-            self.chosenNumberQuestions = chosenNumberQuestions
-            isGameActive = true
-            isSettingActive = false
-            generateQuestions()
-        }
-        
-        func generateQuestions() {
-            for _ in 1...chosenNumberQuestions {
-                let f1 = Int.random(in: 1...10)
-                let f2 = Int.random(in: 2...12)
-                let question = "\(f1) * \(f2) é "
-                let correctAnswer = f1 * f2
-                questionsBank[question] = correctAnswer
-            }
-            print(questionsBank)
-            questionsBank = [:]
-        }
-        
-        func userAnswersQuestion() {
-        }
     }
-    
-    struct GameView: View {
-        let chosenNumberQuestions: Int
-        @State private var userAnswersQuestion = 0
-        @State private var resposta = 0
         
-        var body: some View {
+        func configureGame() {
+            playingGame = true
+            configuringGame = false
+            gameOver = false
             
-            Text("Digite a resposta certa \(chosenNumberQuestions)")
-            Text("Qual o valor de")
-            //                                                                        Text(questionsBank[0] ?? nil)
-            
-            Section("Resposta") {
+            var count = 0
+            while count < numberOfQuestion {
+                let randomNum = Int.random(in: 0...12)
+                let problem = QuestionAndAnswer(operand1: multiplicationTable, operand2: randomNum)
                 
-                //            TextField("Digite a resposta", value: $resposta, format: .number) {
-                //            }
-                //            .keyboardType(.decimalPad)
+                let questionTexts = problem.questionText
+                let questionAnswer = problem.answer
                 
-                //            Button("Responder")
-                //                .background(.blue)
-                //                .foregroundColor(.white)
-                //                .buttonStyle(.bordered)
+                questionArray.append(questionTexts)
+                answerArray.append(questionAnswer)
+                
+                count += 1
             }
+            print(questionArray)
+            print(answerArray)
         }
-    }
-    
-    struct GameSettingsView: View {
-        @State private var chosenTable = 2
-        @State var chosenNumberQuestions = 0
-        let startGame: (Int) -> Void
         
-        var body: some View {
+    func setQuestion() {
+        if currentQuestion != numberOfQuestion {
+            let displaysQuestion = questionArray[currentQuestion]
+            displayQuestion = displaysQuestion
             
-            VStack {
-                HStack {
-                    Image(systemName: "number.circle")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                    Text("Jogo da taubulada")
-                    Image(systemName: "number.circle")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                }
-                Form {
-                    Section {
-                        Text("Escolha a tabuada desejada")
-                        Stepper("Tabuada do \(chosenTable)", value: $chosenTable, in: 2...12, step: 1)
-                    }
-                    Section {
-                        Text("Quantas perguntas quer responder?")
-                        Picker("\(chosenNumberQuestions) perguntas", selection: $chosenNumberQuestions) {
-                            ForEach(1..<50) {
-                                Text("\($0)")
-                            }
-                        }
-                        
-                        Button("Iniciar!") { startGame(chosenNumberQuestions) }
-                        
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .buttonStyle(.bordered)
-                    }
-                }
-            }
+            let setAnswer = answerArray[currentQuestion]
+            answer = setAnswer
+            
+            userAnswer = 0
+        } else {
+            showAlert = true
+            alertMessage = "Fim do jogo"
+            gameOver = true
         }
     }
-    
-    #Preview {
-        ContentView()
-    }
-    
+        
+        func checkAnswer() {
+            if answer == userAnswer {
+                score += 1
+                alertMessage = "Certo!"
+            } else {
+                score -= 1
+                alertMessage = "Errou, resposta é \(userAnswer)"
+            }
+            currentQuestion += 1
+            showAlert = true
+            setQuestion()
+        }
+        
+        func startNewGames() {
+            currentQuestion = 0
+            numberOfQuestion = 0
+            score = 0
+            playingGame = false
+            configuringGame = true
+        }
+}
+
