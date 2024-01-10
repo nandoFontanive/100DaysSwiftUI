dabe68f201af3f7bb151050eb5aea5d41d369d47
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-10 20:25:09 -0300

    more

---
+++ Moonshot-39/Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,53 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Fernando Fontanive on 10/01/24.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    
+    let mission: Mission
+    let crew: [CrewMember]
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                    width * 0.6
+                }
+                .padding(.top)
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
