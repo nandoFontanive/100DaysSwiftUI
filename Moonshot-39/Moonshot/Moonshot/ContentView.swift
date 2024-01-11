f39f064eaf1f33c1ce0d20160d07ac64d0adfc69
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-11 19:17:34 -0300

    dubious list working

--- Moonshot-39/Moonshot/Moonshot/ContentView.swift
+++ Moonshot-39/Moonshot/Moonshot/ContentView.swift
@@ -11,62 +11,127 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
-    @State private var showingMissionsAsList = false
+    @State private var showingMissionsAsList = true
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-//For a tough challenge, add a toolbar item to ContentView that toggles between showing missions as a grid and as a list.
-
+            //            ScrollView {
+            //For a tough challenge, add a toolbar item to ContentView that toggles between showing missions as a grid and as a list.
+            if showingMissionsAsList {
                 List {
-                LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
                         NavigationLink {
                             MissionView(mission: mission, astronauts: astronauts)
                         } label: {
                             VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
                                         .foregroundStyle(.white)
-                                    
-                                    Text(mission.formattedLauchDate)
-                                        .font(.caption)
-                                        .foregroundStyle(.white.opacity(0.5))
                                 }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
                             }
-                            .clipShape(.rect(cornerRadius: 10))
-                            .overlay(RoundedRectangle(cornerRadius: 10)
-                                .stroke(.lightBackground)
-                            )
                         }
                     }
                 }
-                .padding([.horizontal, .bottom])
-            }
-            .navigationTitle("Moonshot")
-            .background(.darkBackground)
-            .preferredColorScheme(.dark)
-            .toolbar {
-                Button("Change view", systemImage: "rectangle.grid.1x2.fill") {
-                    showingMissionsAsList = true
+                
+                
+                //                LazyVGrid(columns: columns) {
+                //                    ForEach(missions) { mission in
+                //                        NavigationLink {
+                //                            MissionView(mission: mission, astronauts: astronauts)
+                //                        } label: {
+                //                            VStack {
+                //                                Image(mission.image)
+                //                                    .resizable()
+                //                                    .scaledToFit()
+                //                                    .frame(width: 100, height: 100)
+                //                                    .padding()
+                //
+                //                                VStack {
+                //                                    Text(mission.displayName)
+                //                                        .font(.headline)
+                //                                        .foregroundStyle(.white)
+                //
+                //                                    Text(mission.formattedLauchDate)
+                //                                        .font(.caption)
+                //                                        .foregroundStyle(.white.opacity(0.5))
+                //                                }
+                //                                .padding(.vertical)
+                //                                .frame(maxWidth: .infinity)
+                //                                .background(.lightBackground)
+                //                            }
+                //                            .clipShape(.rect(cornerRadius: 10))
+                //                            .overlay(RoundedRectangle(cornerRadius: 10)
+                //                                .stroke(.lightBackground)
+                //                            )
+                //                        }
+                //                    }
+                //                }
+                //                .padding([.horizontal, .bottom])
+                
+                .navigationTitle("Moonshot")
+                .background(.darkBackground)
+                .preferredColorScheme(.dark)
+                .toolbar {
+                    Button("Change view", systemImage: "rectangle.grid.1x2.fill") {
+                        showingMissionsAsList = false
+                    }
                 }
             }
+            else {
+                            ScrollView {
+                                LazyVGrid(columns: columns) {
+                                    ForEach(missions) { mission in
+                                        NavigationLink {
+                                            MissionView(mission: mission, astronauts: astronauts)
+                                        } label: {
+                                            VStack {
+                                                Image(mission.image)
+                                                    .resizable()
+                                                    .scaledToFit()
+                                                    .frame(width: 100, height: 100)
+                                                    .padding()
+                
+                                                VStack {
+                                                    Text(mission.displayName)
+                                                        .font(.headline)
+                                                        .foregroundStyle(.white)
+                
+                                                    Text(mission.formattedLauchDate)
+                                                        .font(.caption)
+                                                        .foregroundStyle(.white.opacity(0.5))
+                                                }
+                                                .padding(.vertical)
+                                                .frame(maxWidth: .infinity)
+                                                .background(.lightBackground)
+                                            }
+                                            .clipShape(.rect(cornerRadius: 10))
+                                            .overlay(RoundedRectangle(cornerRadius: 10)
+                                                .stroke(.lightBackground)
+                                            )
+                                        }
+                                    }
+                                }
+                                .padding([.horizontal, .bottom])
+                
+                .navigationTitle("Moonshot")
+                .background(.darkBackground)
+                .preferredColorScheme(.dark)
+                .toolbar {
+                    Button("Change view", systemImage: "rectangle.grid.1x2.fill") {
+                        showingMissionsAsList = true
+                    }
+                }
+            }
+
+            }
+        }
         }
     }
-}
+
 
 
 #Preview {
