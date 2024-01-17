c7ceb25b85cdd8aab01fd7b33a5781e6c44e5013
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-17 19:37:35 -0300

    next day

--- CupcakeCorner-49/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner-49/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -58,27 +58,28 @@
  }
  */
 /*
-import SwiftUI
+ import SwiftUI
+ 
+ struct ContentView: View {
+ @State private var username = ""
+ @State private var email = ""
+ 
+ var body: some View {
+ Form {
+ Section {
+ TextField("Username", text: $username)
+ TextField("Email", text: $email)
+ }
+ 
+ Section {
+ Button("Create account") {
+ print("Creating account…")
+ }
+ }
+ .disabled(username.isEmpty || email.isEmpty)
+ 
+ }
+ }
+ }
+ */
 
-struct ContentView: View {
-    @State private var username = ""
-    @State private var email = ""
-
-    var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-
-            Section {
-                Button("Create account") {
-                    print("Creating account…")
-                }
-            }
-            .disabled(username.isEmpty || email.isEmpty)
-
-        }
-    }
-}
-*/
