010dda27ad348fdac99fd1bec0c099335a8f0b45
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-17 14:39:00 -0300

    cup

---
+++ CupcakeCorner-49/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Fernando Fontanive on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
