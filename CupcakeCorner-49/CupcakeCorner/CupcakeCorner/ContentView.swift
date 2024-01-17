d2efd84bd7be72611d000e7cb96fad145c2a0d78
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-17 15:49:29 -0300

    more

--- CupcakeCorner-49/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner-49/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -4,54 +4,81 @@
 //
 //  Created by Fernando Fontanive on 17/01/24.
 //
-
+/*
+ import SwiftUI
+ 
+ struct Response: Codable {
+ var results: [Result]
+ }
+ 
+ struct Result: Codable {
+ var trackId: Int
+ var trackName: String
+ var collectionName: String
+ }
+ 
+ struct ContentView: View {
+ @State private var results = [Result]()
+ 
+ var body: some View {
+ List(results, id: \.trackId) { item in
+ VStack(alignment: .leading) {
+ Text(item.trackName)
+ .font(.headline)
+ Text(item.collectionName)
+ }
+ }
+ .task {
+ await loadData()
+ }
+ }
+ 
+ 
+ func loadData() async {
+ guard let url = URL(string: "https://itunes.apple.com/search?term=jack+johnson&entity=song") else {
+ print("Invalid URL")
+ return
+ }
+ 
+ do {
+ let (data, _) = try await URLSession.shared.data(from: url)
+ 
+ if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data)
+ {
+ results = decodedResponse.results
+ }
+ } catch {
+ print("Invalid data")
+ }
+ }
+ }
+ 
+ #Preview {
+ ContentView()
+ }
+ */
+/*
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackID: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    
+    @State private var username = ""
+    @State private var email = ""
+
     var body: some View {
-        List(results, id: \.trackID) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName)
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        }
-        .task {
-            await loadData()
-        }
-    }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
             }
-        } catch {
-            print("Invalid data")
+            .disabled(username.isEmpty || email.isEmpty)
+
         }
     }
 }
-        
-        #Preview {
-            ContentView()
-        }
+*/
