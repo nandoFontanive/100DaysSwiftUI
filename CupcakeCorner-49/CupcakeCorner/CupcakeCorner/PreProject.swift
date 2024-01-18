3fee0b8d6d03574f23536854eb40b4fdaa128b9d
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-18 09:49:10 -0300

    ok

---
+++ CupcakeCorner-49/CupcakeCorner/CupcakeCorner/PreProject.swift
@@ -0,0 +1,114 @@
+//
+//  PreProject.swift
+//  CupcakeCorner
+//
+//  Created by Fernando Fontanive on 18/01/24.
+//
+/*
+ 
+ 
+ import SwiftUI
+
+ @Observable
+ class User: Codable {
+     enum CodingKeys: String, CodingKey {
+         case _name = "nome"
+         case _eyes = "zóio"
+     }
+     var name = "Taylor"
+     var eyes = "Black"
+ }
+
+ struct ContentView: View {
+     var body: some View {
+         Button("Encode Taylor", action: encodeTaylor)
+     }
+     
+     func encodeTaylor() {
+         let data = try! JSONEncoder().encode(User())
+         let str = String(decoding: data, as: UTF8.self)
+         print(str)
+     }
+ }
+
+
+ /*
+  struct Response: Codable {
+  var results: [Result]
+  }
+  
+  struct Result: Codable {
+  var trackId: Int
+  var trackName: String
+  var collectionName: String
+  }
+  
+  struct ContentView: View {
+  @State private var results = [Result]()
+  
+  var body: some View {
+  List(results, id: \.trackId) { item in
+  VStack(alignment: .leading) {
+  Text(item.trackName)
+  .font(.headline)
+  Text(item.collectionName)
+  }
+  }
+  .task {
+  await loadData()
+  }
+  }
+  
+  
+  func loadData() async {
+  guard let url = URL(string: "https://itunes.apple.com/search?term=jack+johnson&entity=song") else {
+  print("Invalid URL")
+  return
+  }
+  
+  do {
+  let (data, _) = try await URLSession.shared.data(from: url)
+  
+  if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data)
+  {
+  results = decodedResponse.results
+  }
+  } catch {
+  print("Invalid data")
+  }
+  }
+  }
+  
+  #Preview {
+  ContentView()
+  }
+  */
+ /*
+  import SwiftUI
+  
+  struct ContentView: View {
+  @State private var username = ""
+  @State private var email = ""
+  
+  var body: some View {
+  Form {
+  Section {
+  TextField("Username", text: $username)
+  TextField("Email", text: $email)
+  }
+  
+  Section {
+  Button("Create account") {
+  print("Creating account…")
+  }
+  }
+  .disabled(username.isEmpty || email.isEmpty)
+  
+  }
+  }
+  }
+  */
+
+
+ 
+ */
