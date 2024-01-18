6116a9b1f9169852513e44b603e8a2cedf61b71b
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-18 10:27:10 -0300

    .

--- CupcakeCorner-49/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner-49/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var order = Order()
+    
     var body: some View {
-        Text("")
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...10)
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
     }
 }
 
--- CupcakeCorner-49/CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner-49/CupcakeCorner/CupcakeCorner/Order.swift
@@ -7,6 +7,18 @@
 
 import SwiftUI
 
+@Observable
+Class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false
+    var extraFrosting = false
+    var addSprinkles = false
+}
+
 struct Order: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
