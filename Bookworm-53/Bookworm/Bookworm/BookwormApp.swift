ad3282b2c4945117af68e707977cb569f8c6009e
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-19 19:34:26 -0300

    .

---
+++ Bookworm-53/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Fernando Fontanive on 19/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
