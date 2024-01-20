ab195467416b30ef37abf5fa25c5be9fd5bee79a
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-20 17:17:05 -0300

    p1 done

--- Bookworm-53/Bookworm/Bookworm/ContentView.swift
+++ Bookworm-53/Bookworm/Bookworm/ContentView.swift
@@ -5,17 +5,29 @@
 //  Created by Fernando Fontanive on 20/01/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    @State private var isShowingAddBookView = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add Book", systemImage: "plus") {
+                            isShowingAddBookView.toggle()
+                        }
+                    }
+                }
+                .sheet(isPresented: $isShowingAddBookView) {
+                    AddBookView()
+                }
         }
-        .padding()
     }
 }
 
