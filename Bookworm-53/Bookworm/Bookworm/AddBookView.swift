38194ce65c5586bbf0846defea0cbf7e59a52467
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-21 15:16:51 -0300

    emoji

--- Bookworm-53/Bookworm/Bookworm/AddBookView.swift
+++ Bookworm-53/Bookworm/Bookworm/AddBookView.swift
@@ -35,12 +35,8 @@ struct AddBookView: View {
                 
                 Section("Write a review") {
                     TextEditor(text: $review)
+                    RatingView(rating: $rating)
                     
-                    Picker("Rating", selection: $rating) {
-                        ForEach(0..<6) {
-                            Text(String($0))
-                        }
-                    }
                 }
                 Section {
                     Button("Save") {
