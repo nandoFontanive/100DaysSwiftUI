e92cdb69128e4e888616ab8e6a4ea038f1c0b401
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-22 19:51:07 -0300

    Challenges start

--- Bookworm-53/Bookworm/Bookworm/ContentView.swift
+++ Bookworm-53/Bookworm/Bookworm/ContentView.swift
@@ -25,10 +25,11 @@ struct ContentView: View {
                         HStack {
                             EmojiRatingView(rating: book.rating)
                                 .font(.largeTitle)
-                            
                             VStack(alignment: .leading) {
                                 Text(book.title)
                                     .font(.headline)
+                                    .foregroundStyle(EmojiRatingView(rating: 2) ? .red : .primary)
+                                    
                                 Text(book.author)
                                     .foregroundStyle(.secondary)
                             }
