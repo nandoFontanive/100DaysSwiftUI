d928dd717990285ee7dd87bb662fb9486911cf43
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-23 13:32:00 -0300

    new proj start

---
+++ SwiftDataProject-57/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Fernando Fontanive on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
