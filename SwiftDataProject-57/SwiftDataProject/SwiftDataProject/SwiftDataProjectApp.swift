bb39af61627d9e23ff62e00838f9a817de0dc611
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-23 14:02:52 -0300

    .

--- SwiftDataProject-57/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject-57/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Fernando Fontanive on 23/01/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
