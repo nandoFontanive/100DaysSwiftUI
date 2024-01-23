364299feccaaa01845030e41869288cb767d5834
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-23 17:34:33 -0300

    .

---
+++ SwiftDataProject-57/SwiftDataProject/SwiftDataProject/EditUserView.swift
@@ -0,0 +1,22 @@
+//
+//  EditUserView.swift
+//  SwiftDataProject
+//
+//  Created by Fernando Fontanive on 23/01/24.
+//
+
+import SwiftUI
+
+struct EditUserView: View {
+    @Bindable var user: User
+
+    var body: some View {
+        Form {
+            TextField("Name", text: $user.name)
+            TextField("City", text: $user.city)
+            DatePicker("Join Date", selection: $user.joinDate)
+        }
+        .navigationTitle("Edit User")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
