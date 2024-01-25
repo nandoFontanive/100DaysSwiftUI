e10ebcf6f1227691842378079f547ba118613496
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-25 10:38:42 -0300

    .

--- SwiftDataProject-57/SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject-57/SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -10,55 +10,58 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-//    @Query(filter: #Predicate<User> { user in
-//        if user.name.localizedStandardContains("R") {
-//            if user.city == "London" {
-//                return true
-//            } else {
-//                return false
-//            }
-//        } else {
-//            return false
-//        }
-//    }, sort: \User.name) var users: [User]
+    //    @Query(filter: #Predicate<User> { user in
+    //        if user.name.localizedStandardContains("R") {
+    //            if user.city == "London" {
+    //                return true
+    //            } else {
+    //                return false
+    //            }
+    //        } else {
+    //            return false
+    //        }
+    //    }, sort: \User.name) var users: [User]
     //    @State private var path = [User]()
     @State private var showingUpcomingOnly = false
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate)]
     
     
     var body: some View {
         //        NavigationStack(path: $path) {
         NavigationStack {
-            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast)
-//            List(users) { user in
-                //                NavigationLink(value: user) {
-//                Text(user.name)
-            }
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
+            //            List(users) { user in
+            //                NavigationLink(value: user) {
+            //                Text(user.name)
+            //            }
             
-            .navigationTitle("Users")
+                .navigationTitle("Users")
             //            .navigationDestination(for: User.self) { user in
             //                EditUserView(user: user)
-            .toolbar {
-                Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
-                    showingUpcomingOnly.toggle()
+                .toolbar {
+                    Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                        showingUpcomingOnly.toggle()
+                    }
+                    Button("Add Samples", systemImage: "plus") {
+                        try? modelContext.delete(model: User.self)
+                        let first = User(name: "Ed", city: "Landan", joinDate: .now.addingTimeInterval(86400 * -10))
+                        let second = User(name: "Rosa", city: "London", joinDate: .now.addingTimeInterval(86400 * -5))
+                        let third = User(name: "Jon", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                        let fourth = User(name: "Sansa", city: "Kiev", joinDate: .now.addingTimeInterval(86400 * 10))
+                        //                            let user = User(name: "", city: "", joinDate: .now)
+                        //                            modelContext.insert(user)
+                        //                            path = [user]
+                        modelContext.insert(first)
+                        modelContext.insert(second)
+                        modelContext.insert(third)
+                        modelContext.insert(fourth)
+                    }
                 }
-                Button("Add Samples", systemImage: "plus") {
-                    try? modelContext.delete(model: User.self)
-                    let first = User(name: "Ed", city: "Landan", joinDate: .now.addingTimeInterval(86400 * -10))
-                    let second = User(name: "Rosa", city: "London", joinDate: .now.addingTimeInterval(86400 * -5))
-                    let third = User(name: "Jon", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
-                    let fourth = User(name: "Sansa", city: "Kiev", joinDate: .now.addingTimeInterval(86400 * 10))
-                    //                            let user = User(name: "", city: "", joinDate: .now)
-                    //                            modelContext.insert(user)
-                    //                            path = [user]
-                    modelContext.insert(first)
-                    modelContext.insert(second)
-                    modelContext.insert(third)
-                    modelContext.insert(fourth)
-                }
-            }
         }
     }
-
+}
 
 #Preview {
     ContentView()
--- SwiftDataProject-57/SwiftDataProject/SwiftDataProject/UsersView.swift
+++ SwiftDataProject-57/SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -13,10 +13,10 @@ import SwiftUI
 struct UsersView: View {
     @Query var users: [User]
     
-    init(minimumJoinDate: Date) {
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
-        }, sort: \User.name)
+        }, sort: sortOrder)
     }
 
 
@@ -31,6 +31,6 @@ struct UsersView: View {
 }
 
 #Preview {
-    UsersView(minimumJoinDate: .now)
+    UsersView(minimumJoinDate: .now, sortOrder: [SortDescriptor(\User.name)])
         .modelContainer(for: User.self)
 }
