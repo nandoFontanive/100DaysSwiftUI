3d2ad1fe4b4e3379f1e714b562bdc9b5efac9b49
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-26 14:44:13 -0300

    .

--- SwiftDataProject-57/SwiftDataProject/SwiftDataProject/UsersView.swift
+++ SwiftDataProject-57/SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -11,22 +11,42 @@ import SwiftUI
 
 
 struct UsersView: View {
+    @Environment(\.modelContext) var modelContext
     @Query var users: [User]
     
+    var body: some View {
+            List(users) { user in
+                HStack {
+                Text(user.name)
+                Spacer()
+                Text(String(user.jobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+
+            }
+        }
+            .onAppear(perform: addSample)
+        
+    }
     init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
         }, sort: sortOrder)
     }
-
-
     
-    var body: some View {
-
-        List(users) { user in
-            Text(user.name)
-            
-        }
+    func addSample() {
+        let user1 = User(name: "Piper", city: "New York", joinDate: .now)
+        let job1 = Job(name: "Organize", priority: 3)
+        let job2 = Job(name: "Make plans", priority: 4)
+        
+        modelContext.insert(user1)
+        
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
     }
 }
 
