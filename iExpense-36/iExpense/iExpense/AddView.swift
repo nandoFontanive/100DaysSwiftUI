2178ebad3b97722c3de783321fa1a1f89445f619
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-27 16:53:25 -0300

    .

--- iExpense-36/iExpense/iExpense/AddView.swift
+++ iExpense-36/iExpense/iExpense/AddView.swift
@@ -53,7 +53,7 @@ struct AddView: View {
                     Button("Save") {
                         let item = ExpenseItem(name: name, type: type, amount: amount)
 //                        expenses.items.append(item)
-                        modelContext.insert(expenses)
+                        modelContext.insert(item)
                         dismiss()
                     }
                 }
