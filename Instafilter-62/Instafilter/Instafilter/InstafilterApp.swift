df6274f50ef1e8fae361d16394982d5fdee43414
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-29 19:10:56 -0300

    Instafilter start

---
+++ Instafilter-62/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Fernando Fontanive on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
