236b89615ffdd52cd20b5609d91ba33cb97a7c21
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-30 17:58:22 -0300

    .

--- Instafilter-62/Instafilter/Instafilter/ContentView.swift
+++ Instafilter-62/Instafilter/Instafilter/ContentView.swift
@@ -26,11 +26,18 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
         
         let context = CIContext()
-        let currentFilter = CIFilter.pixellate()
-        
+        let currentFilter = CIFilter.crystallize()
         currentFilter.inputImage = beginImage
-//        currentFilter.intensity = 1
-        currentFilter.scale = 20
+
+        let amount = 1.0
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+        
+        
         
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
