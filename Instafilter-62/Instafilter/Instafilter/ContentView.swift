3a023f49e984b1561db3be3301fd29aa86bfe8ac
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-01-30 17:48:08 -0300

    .

--- Instafilter-62/Instafilter/Instafilter/ContentView.swift
+++ Instafilter-62/Instafilter/Instafilter/ContentView.swift
@@ -5,17 +5,38 @@
 //  Created by Fernando Fontanive on 29/01/24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFill()
         }
-        .padding()
+        .onAppear(perform: loadImage)
+    }
+
+    func loadImage() {
+        let inputImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.pixellate()
+        
+        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1
+        currentFilter.scale = 20
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 
