22073066b8a4b893598b2a8b2d533049c179e26c
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-02-01 11:40:58 -0300

    Update ContentView.swift

--- Instafilter-62/Instafilter/Instafilter/ContentView.swift
+++ Instafilter-62/Instafilter/Instafilter/ContentView.swift
@@ -145,5 +145,11 @@ struct ContentView: View {
     }
     func applyProcessing() {
         currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
