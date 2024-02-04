d9cedddf6bf9c81d45267eea03cc3adc0afe74b3
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-02-04 16:18:03 -0300

    Bucket start

---
+++ BucketList-68/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Fernando Fontanive on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
