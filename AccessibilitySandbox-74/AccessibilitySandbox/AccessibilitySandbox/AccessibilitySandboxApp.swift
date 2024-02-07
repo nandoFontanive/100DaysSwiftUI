45b1e067671bde49527bc2901a563e5784b89160
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-02-07 15:38:56 -0300

    .

---
+++ AccessibilitySandbox-74/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Fernando Fontanive on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
