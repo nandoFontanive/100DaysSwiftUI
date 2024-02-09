f1a7664bd69a5a5e6039f22d18f1ffc7ea258ee1
nandoFontanive <107313489+nandoFontanive@users.noreply.github.com>
2024-02-09 14:57:53 -0300

    .

--- BucketList-68/BucketList/BucketList/ContentView.swift
+++ BucketList-68/BucketList/BucketList/ContentView.swift
@@ -5,22 +5,50 @@
 //  Created by Fernando Fontanive on 04/02/24.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
     @State private var viewModel = ViewModel()
-
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if viewModel.isUnlocked {
+            MapReader { proxy in
+                Map(initialPosition: startPosition) {
+                    forEach(viewModel.locations) { location in
+                        Annotation(location.name, coordinate: location.coordinate) {
+                            Image(Image(systemName: "star.circle"))
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(.circle)
+                                .onLongPressGesture {
+                                    viewModel.selectedPlace = location
+                                }
+                        }
+                    }
+                }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addLocation(at: coordinate)
+                    }
+                }
+                .sheet(item: $viewModel.selectedPlace) in
+                EditView(location: place) {
+                    viewModel.update(location: $0)
+                }
+            }
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
-}
+    else {
+        //Button here
+    }
+    
+    
+    
+    
+    
+    #Preview {
+        ContentView()
+    }
