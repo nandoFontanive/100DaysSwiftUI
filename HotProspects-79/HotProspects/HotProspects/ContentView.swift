dd372ccb84fe1ed9cadd5f875842e0dcb887b7d1
Fernando Fontanive <nando.fontanive@gmail.com>
2024-10-13 16:58:10 -0300

    .

--- HotProspects-79/HotProspects/HotProspects/ContentView.swift
+++ HotProspects-79/HotProspects/HotProspects/ContentView.swift
@@ -8,30 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab = "One"
-    
+    @State private var backGroundColor = Color.yellow
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Button("Show Tab 2") {
-                selectedTab = "dois"
-            }
-            .tabItem {
-                Label("One", systemImage: "star")
-            }
-            .tag("um")
-            
-            Text("Tab 2")
-                .tabItem {
-                    Label("Two", systemImage: "globe")
+        List {
+            Text("Taylor")
+                .swipeActions {
+                    Button("Delete", systemImage: "minus.circle", role: .destructive) {
+                        print("Delete")
+                    }
                 }
-                .tag("dois")
-            
+                .swipeActions(edge: .leading) {
+                    Button("Pin", systemImage: "pin") {
+                        print("Pinning")
+                    }
+                    .tint(.orange)
+                }
+                
         }
-        
+    
     }
 }
-
 #Preview {
     ContentView()
 }
+//        day 81
+//        Text("Hellot!")
+//            .padding()
+//            .background(backGroundColor)
+//        Text("Change background color")
+//            .padding()
+//            .background(.red)
+//            .contextMenu {
+//                Button("red", systemImage: "paperplane.fill", role: .destructive) {
+//                    backGroundColor = .red
+//                }
+//                Button("blue", systemImage: "checkmark.circle") {
+//                    backGroundColor = .blue
+//                }
+//                Button("gray") {
+//                    backGroundColor = .gray
+//                }
+//            }
+    //        day 80 - result
+    //        Text(output)
+    //            .task {
+    //                await fetchReadings()
+    //            }
+    //
+    //    }
+    //
+    //    func fetchReadings() async {
+    //        let fetchTask = Task {
+    //            let url = URL(string: "https://hws.dev/readings.json")!
+    //            let (data, _) = try await URLSession.shared.data(from: url)
+    //            let readings = try JSONDecoder().decode([Double].self, from: data)
+    //            return "Found \(readings.count) readings"
+    //        }
+    //        let resultado = await fetchTask.result
+    //        switch resultado {
+    //            case .success(let str):
+    //                output = str
+    //            case .failure(let error):
+    //                output = "Error: \(error.localizedDescription)"
+    //        }
+    //
+    ////        do {
+    ////            output = try resultado.get()
+    ////        } catch {
+    ////            output = "Error: \(error.localizedDescription)"
+    ////        }
+    //
+    //
+    //    }
+//day 79
+//    var body: some View {
+//        TabView(selection: $selectedTab) {
+//            Button("Show Tab 2") {
+//                selectedTab = "dois"
+//            }
+//            .tabItem {
+//                Label("One", systemImage: "star")
+//            }
+//            .tag("um")
+//
+//            Text("Tab 2")
+//                .tabItem {
+//                    Label("Two", systemImage: "globe")
+//                }
+//                .tag("dois")
+//
+//        }
+//
+//    }
