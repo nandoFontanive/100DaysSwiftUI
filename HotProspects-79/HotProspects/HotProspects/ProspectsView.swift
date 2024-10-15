9f090c8ff02a07e4b9290d9b612b0f08d0e20587
Fernando Fontanive <nando.fontanive@gmail.com>
2024-10-15 11:12:00 -0300

    .

--- HotProspects-79/HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects-79/HotProspects/HotProspects/ProspectsView.swift
@@ -5,12 +5,19 @@
 //  Created by Fernando Fontanive on 14/10/24.
 //
 
+import CodeScanner
+import SwiftData
 import SwiftUI
 
 struct ProspectsView: View {
     enum FilterType {
         case none, contacted, uncontacted
     }
+    
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \Prospect.name) var prospects: [Prospect]
+    @State private var isShowingScanner = false
+    @State private var selectedProspects = Set<Prospect>()
     
     let filter: FilterType
     
@@ -27,13 +34,95 @@ struct ProspectsView: View {
     
     var body: some View {
         NavigationStack {
-            Text("Hello!")
-                .navigationTitle(title)
+            List(prospects, selection: $selectedProspects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
+                }
+                .swipeActions {
+                    Button("Delete", systemImage: "trash", role: .destructive) {
+                        modelContext.delete(prospect)
+                    }
+                    if prospect.isContacted {
+                        Button("Mark uncontacted", systemImage: "person.crop.circle.badge.xmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue)
+                    }
+                    else {
+                        Button("Mark contacted", systemImage: "person.crop.fill.badge.checkmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green)
+                    }
+                }
+                .tag(prospect)
+            }
+            .navigationTitle(title)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Scan", systemImage: "qrcode.viewfinder") {
+                        isShowingScanner = true
+                    }
+                }
+                    
+                    ToolbarItem(placement: .topBarLeading) {
+                        EditButton()
+                    }
+                    
+                    if selectedProspects.isEmpty == false {
+                        ToolbarItem(placement: .bottomBar) {
+                            Button("Delete Selected", action: delete)
+                        }
+                    }
+                }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Sigmund\nfsigmund@gmail.com", completion: handleScan)
+            }
+        }
+    
+    
+    init(filter: FilterType) {
+        self.filter = filter
+        
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
             
+            _prospects = Query(filter: #Predicate {
+                $0.isContacted == showContactedOnly
+                
+            }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return } //check if only 2 details were scanned
+        
+            let person = Prospect(name: details[0], email: details[1], isContacted: false)
+            modelContext.insert(person) //insert into swiftdata
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
         }
     }
 }
-    
-    #Preview {
-        ProspectsView(filter: .none)
-    }
+
+
+
+#Preview {
+    ProspectsView(filter: .none)
+        .modelContainer(for: Prospect.self)
+}
