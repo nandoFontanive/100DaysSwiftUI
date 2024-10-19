f29712314b43a0321caa530f385fe14b7d489932
Fernando Fontanive <nando.fontanive@gmail.com>
2024-10-19 16:45:50 -0300

    .

--- Flashzilla-86/Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla-86/Flashzilla/Flashzilla/CardView.swift
@@ -8,15 +8,28 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
     @State private var offset = CGSize.zero
     @State private var isShowingAnswer = false
+    
     let card: Card
     var removal: (() -> Void)? = nil
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
-                .fill(.white)
+                .fill(
+                    accessibilityDifferentiateWithoutColor
+                    ? .white
+                    : .white
+                        .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(
+                    accessibilityDifferentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             
             VStack {
@@ -30,8 +43,8 @@ struct CardView: View {
                         .foregroundStyle(.secondary)
                 }
             }
-                    .padding(20)
-                    .multilineTextAlignment(.center)
+            .padding(20)
+            .multilineTextAlignment(.center)
         }
         .frame(width: 400, height: 250)
         .rotationEffect(.degrees(offset.width / 5.0))
@@ -40,20 +53,20 @@ struct CardView: View {
         .gesture(
             
             DragGesture()
-            .onChanged { gesture in
-                offset = gesture.translation
-            }
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
                 .onEnded { _ in //ignore what's coming in
                     if abs(offset.width) > 100 {
                         removal?()
                     }
-                        else {
-                            offset = .zero
-                        }
+                    else {
+                        offset = .zero
                     }
-                )
-            
-            
+                }
+        )
+        
+        
         
         .onTapGesture {
             isShowingAnswer.toggle()
--- Flashzilla-86/Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla-86/Flashzilla/Flashzilla/ContentView.swift
@@ -15,7 +15,14 @@ extension View {
 }
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
     
     var body: some View {
         ZStack {
@@ -23,6 +30,13 @@ struct ContentView: View {
                 .resizable()
                 .ignoresSafeArea()
             VStack {
+                Text("Timer: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -34,7 +48,44 @@ struct ContentView: View {
                     }
                 }
             }
+            if accessibilityDifferentiateWithoutColor {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    }
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
         }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            }
+            else {
+                isActive = false
+            }
+        }
+            
     }
     func removeCard(at index: Int) {
         cards.remove(at: index)
