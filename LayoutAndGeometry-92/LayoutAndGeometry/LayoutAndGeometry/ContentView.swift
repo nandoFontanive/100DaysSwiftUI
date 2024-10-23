9eb6c7bfaf138e60bdd88d4d7c350f4c9bc66b7b
Fernando Fontanive <nando.fontanive@gmail.com>
2024-10-23 13:58:29 -0300

    .

---
+++ LayoutAndGeometry-92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Fernando Fontanive on 23/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
+//        VStack(alignment: .leading) {
+//            ForEach(0..<10) { position in
+//                Text("Number \(position)")
+//                    .alignmentGuide(.leading) { _ in Double(position) * -10 }
+//            }
+//        }
+//        .background(.green)
+//        .frame(width: 400, height: 400)
+//        .background(.blue)
