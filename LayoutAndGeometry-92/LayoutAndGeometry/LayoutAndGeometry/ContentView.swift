8eb663f4240c550c2d8ff0adc465f19671c70707
Fernando Fontanive <nando.fontanive@gmail.com>
2024-10-24 11:03:47 -0300

    .

--- LayoutAndGeometry-92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry-92/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,8 +7,35 @@
 
 import SwiftUI
 
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+
 struct ContentView: View {
     var body: some View {
+        HStack {
+            Text("IMPORTANT")
+                .frame(width: 200)
+                .background(.blue)
+
+
+            GeometryReader { proxy in
+                Image(.turma)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.8)
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+            }
+
+        }
+
     }
 }
 
@@ -16,6 +43,25 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+//        Text("Hello, world!")
+//            .offset(x: 100, y: 100)
+//            .background(.red)
+//        HStack(alignment: .midAccountAndName) {
+//            VStack {
+//                Text("@twostraws")
+//                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+//                Image(.turma)
+//                    .resizable()
+//                    .frame(width: 64, height: 64)
+//            }
+//
+//            VStack {
+//                Text("Full name:")
+//                Text("PAUL HUDSON")
+//                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+//                    .font(.largeTitle)
+//            }
+//        }
 //        VStack(alignment: .leading) {
 //            ForEach(0..<10) { position in
 //                Text("Number \(position)")
